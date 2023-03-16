775fbde9fe819de77a319cfe4420b1dcc820423a
samniyonzima1 <samniyonzima1@gmail.com>
2023-03-16 10:12:22 -0700

    puts in action

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ * main -using a puts function to write a program
+ * Return: 0 (success)
+ */
+int main(void)
+{
+puts("\ "Programming is like building a multilingual puzzle");
+return (0);
+}
