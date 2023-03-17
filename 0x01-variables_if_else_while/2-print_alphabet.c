20f2dfa87e1e94a6e3de1cb540ef5c5c38eea49b
samniyonzima1 <samniyonzima1@gmail.com>
2023-03-17 12:54:31 -0700

    printing alphabets in lower case

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+/**
+ * main - Prints the  alphabet.
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+
+}
