5da19f4e034a0da745e324c63da942e2045e8c49
samniyonzima1 <samniyonzima1@gmail.com>
2023-03-17 17:42:07 -0700

    positive single inger-numbers  printed

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -7,9 +7,11 @@
 
 int main(void)
 {
-	int n;
+	int i;
 
-	for (n = '0'; n <= '9'; n++)
-		printf(n);
-	return (int);
+	for (i = 0; i < 10; i++)
+	printf("%d", i);
+	printf("\n");
+
+	return (0);
 }
