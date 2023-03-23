cc85ea7b2e47985bd091423aaa12edd14c7fcb2f
samniyonzima1 <samniyonzima1@gmail.com>
2023-03-23 17:20:43 +0000

    fizz buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,17 +11,17 @@ int main(void)
 
 	for (x = 1; x <= 100; x++)
 	{
-		if (x % 3 == 0)
+		if ((x % 3 == 0) && (x % 5 == 0))
+		{
+			printf("FizzBuzz ");
+		}
+		else if (x % 3 == 0)
 		{
 			printf("Fizz ");
 		}
 		else if (x % 5 == 0)
 		{
 			printf("Buzz ");
-		}
-		else if ((x % 3 == 0) && (x % 5 == 0))
-		{
-			printf("FizzBuzz ");
 		}
 		else
 		{
@@ -30,4 +30,4 @@ int main(void)
 	}
 	printf("\n");
 	return (0);
-}		
+}
