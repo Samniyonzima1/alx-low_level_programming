b116b6c0687abe99ccb853f307d21a5164d7eb30
samniyonzima1 <samniyonzima1@gmail.com>
2023-03-23 15:18:50 +0000

    print most numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,24 @@
+#include"main.h"
+
+/*
+ * print_most_numbers - prints all numbers except 4 and 2 below 9
+ * Return: 0 on success
+ */
+
+void print_most_numbers(void)
+{
+	int c = 0;
+	
+	for (; c <= 9; c++)
+	{
+	if (c == 2 || c == 4)
+	{
+	continue;
+	}
+	else
+	{
+	_putchar(c + '0');
+	}
+	}
+	_putchar('\n');
+}
