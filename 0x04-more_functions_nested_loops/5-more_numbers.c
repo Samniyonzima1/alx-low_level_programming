06ced9349bb92948f50a32d23ae6ea54cd4bb96d
samniyonzima1 <samniyonzima1@gmail.com>
2023-03-23 15:39:09 +0000

    more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include"main.h"
+/**
+ * more_numbers - prints all numbers below 14
+ * Return: 0 success
+ */
+
+void more_numbers(void)
+
+{
+	int x, y;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (y = 0; y <= 14; y++)
+		{
+			if (y > 0)
+			{
+				_putchar((y / 10) + '0');
+			}
+			_putchar((y % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
