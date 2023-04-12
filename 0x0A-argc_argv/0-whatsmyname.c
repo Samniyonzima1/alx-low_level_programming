faa0750c1156d08cb3654fcac49274eda176a6bd
samniyonzima1 <samniyonzima1@gmail.com>
2023-04-12 15:22:47 +0000

    argc and argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#pragma GCC diagnostic ignored "-Wunused-parameter"
+/**
+ * main - program that prints its name
+ * @argc: number of arrays
+ * @argv: string to be used
+ * Return: 0 always
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("My name is: %s\n", argv[0]);
+	return (0);
+}
