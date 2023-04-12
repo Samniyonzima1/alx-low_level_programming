92a88983681db724357bfea53d5cb2784d5f5e02
samniyonzima1 <samniyonzima1@gmail.com>
2023-04-12 15:40:27 +0000

    numer of arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#pragma GCC diagnostic ignored "-Wunused-parameter"
+
+
+/**
+ * main - prints number of arguments passed to the program
+ * @argc: the number of arguments
+ * @argv: string to
+ * Return: 0 always
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
