6dbb8fd35beb3f215ea63b2debe9751e460210e1
samniyonzima1 <samniyonzima1@gmail.com>
2023-04-16 11:17:18 +0000

    string lenght

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,21 @@
+#include"main.h"
+#include<string.h>
+
+/**
+ * _strlen_recursion - prints the length of string
+ * @s: string to be evaluated
+ * Return: 0 on success
+ */
+
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return 0;
+	}
+	else
+	{
+		return 1 + _strlen_recursion(s +1);
+	}
+}
