bb8b198b50ca1385b026873a710c613a535a5bb7
samniyonzima1 <samniyonzima1@gmail.com>
2023-04-17 11:36:15 +0000

    structures definition

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+/**
+ * struct dog - my new structure
+ * @name: first member
+ * @age: second member
+ * @owner: third member
+ *
+ * Description: this is my first structure in ALX
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
