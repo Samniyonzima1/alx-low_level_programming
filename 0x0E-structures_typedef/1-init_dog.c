aa603b4dbf3949e1e2902e5dddb40cae374b134b
samniyonzima1 <samniyonzima1@gmail.com>
2023-04-17 13:14:15 +0000

    typedef

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include"dog.h"
+
+/**
+ * init_dog - initialize my varibles
+ * @d: one variable
+ * @name: second variable
+ * @age: third variable
+ * @owner: last variable
+ *
+ * Return: nothing
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,19 @@
+#include"dog.h"
+
+/**
+ * new_dog - creates new dog struct
+ * @name: ...
+ * @age: ...
+ * @owner: ...
+ * Return: nothing
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	struct new_dog
+	{
+		name;
+		age;
+		owner;
+	};
+
